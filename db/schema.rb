95fd9405ecad999a8897920effe1db119a1d928c
Daniel Torres <denial.torres@gmail.com>
2025-05-31 23:27:12 -0600

    Create initial shops table schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_01_052207) do
+  create_table "shops", force: :cascade do |t|
+    t.string "shopify_domain", null: false
+    t.string "shopify_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "access_scopes", default: "", null: false
+    t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  end
+
 end
